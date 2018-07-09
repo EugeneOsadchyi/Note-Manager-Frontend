7961905573dd2fbe13e2b991b4b132dbc2c00a20
Eugene Osadchyi <eugene.osadchyi@gmail.com>
2018-07-09 16:11:50 +0300

    Add reducer tests for opening selected directory

--- src/store/reducers/directory.test.js
+++ src/store/reducers/directory.test.js
@@ -4,7 +4,7 @@ import * as actionTypes from '../actions/actionTypes';
 import reducer from './directory';
 
 describe('Directory reducer', () => {
-  const initialState = {
+  let initialState = {
     directories: [{
       id: 1, name: 'Closed directory', opened: false, active: false,
     }, {
@@ -70,16 +70,59 @@ describe('Directory reducer', () => {
   });
 
   describe('when selects the directory', () => {
+    beforeEach(() => {
+      initialState = {
+        directories: [{
+          id: 1, name: 'Directory 1', opened: false, active: false,
+        }, {
+          id: 2, name: 'Directory 2', opened: false, active: true,
+        }, {
+          id: 3, name: 'Directory 1.1', opened: false, active: false,
+        }, {
+          id: 4, name: 'Directory 1.2', opened: false, active: false,
+        }, {
+          id: 5, name: 'Directory 1.1.1', opened: false, active: false,
+        }],
+      };
+
+      deepFreeze(initialState);
+    });
+
     it('sets directory `active` property to true and resets other directories `active` property to false', () => {
       const expectedState = {
         directories: [{
-          id: 1, name: 'Closed directory', opened: false, active: true,
+          id: 1, name: 'Directory 1', opened: false, active: true,
         }, {
-          id: 2, name: 'Opened directory', opened: true, active: false,
+          id: 2, name: 'Directory 2', opened: false, active: false,
+        }, {
+          id: 3, name: 'Directory 1.1', opened: false, active: false,
+        }, {
+          id: 4, name: 'Directory 1.2', opened: false, active: false,
+        }, {
+          id: 5, name: 'Directory 1.1.1', opened: false, active: false,
         }],
       };
 
       expect(reducer(initialState, { type: actionTypes.SELECT_DIRECTORY, id: 1 }))
+        .toEqual(expectedState);
+    });
+
+    it('sets parent directories\' `opened` property to true', () => {
+      const expectedState = {
+        directories: [{
+          id: 1, name: 'Directory 1', opened: true, active: false,
+        }, {
+          id: 2, name: 'Directory 2', opened: true, active: false,
+        }, {
+          id: 3, name: 'Directory 1.1', opened: true, active: false,
+        }, {
+          id: 4, name: 'Directory 1.2', opened: false, active: false,
+        }, {
+          id: 5, name: 'Directory 1.1.1', opened: false, active: true,
+        }],
+      };
+
+      expect(reducer(initialState, { type: actionTypes.SELECT_DIRECTORY, id: 5 }))
         .toEqual(expectedState);
     });
   });
